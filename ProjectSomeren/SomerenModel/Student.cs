61ec3fbf49f694c3ec9d1523f3826dc320b84ae6
Zoran Serdic <35991493+ZoranSerdic@users.noreply.github.com>
2023-03-16 21:35:45 +0100

    Updated student with the correct data

--- ProjectSomeren/SomerenModel/Student.cs
+++ ProjectSomeren/SomerenModel/Student.cs
@@ -4,9 +4,12 @@ namespace SomerenModel
 {
     public class Student
     {
-        public int Id { get; set; }     // database id
-        public string Name { get; set; }
-        public int Number { get; set; } // StudentNumber, e.g. 474791
-        public DateTime BirthDate { get; set; }
+        public int student_ID { get; set; }     // database Primary Key
+        public string clas { get; set; }        // written as clas since visual studio will complain about using class
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string telephone_number { get; set; }
+        public int room_ID { get; set; }     
+
     }
 }
