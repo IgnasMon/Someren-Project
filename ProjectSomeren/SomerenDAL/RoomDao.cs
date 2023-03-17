c409f93137687403a3b8710321285d1193efb658
saif <113261960+Saif8122@users.noreply.github.com>
2023-03-17 22:52:58 +0100

    I added Rooms

---
+++ ProjectSomeren/SomerenDAL/RoomDao.cs
@@ -0,0 +1,39 @@
+﻿using SomerenModel;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenDAL
+{
+    public class RoomDao : BaseDao
+    {
+        public List<Room> GetAllRooms()
+        {
+            string query = "SELECT * FROM dbo.rooms";
+            SqlParameter[] sqlParameters = new SqlParameter[0];
+            return ReadTables(ExecuteSelectQuery(query, sqlParameters));
+        }
+
+        private List<Room> ReadTables(DataTable dataTable)
+        {
+            List<Room> rooms = new List<Room>();
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                Room room = new Room()
+                {
+                    room_ID = (int)dr["room_ID"],
+                    capacity = (int)dr["capacity"],
+                    floor = (int)dr["floor"],
+                    building = dr["building"].ToString(),
+                };
+                rooms.Add(room);
+            }
+            return rooms;
+        }
+    }
+}
