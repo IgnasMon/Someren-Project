2aa0a892ff2d97397fc43f0ef90b79eb896fe22e
Zoran Serdic <35991493+ZoranSerdic@users.noreply.github.com>
2023-03-17 22:21:38 +0100

    Student database, UI and modified all other panels to work correctly

--- ProjectSomeren/SomerenDAL/StudentDao.cs
+++ ProjectSomeren/SomerenDAL/StudentDao.cs
@@ -9,7 +9,7 @@ namespace SomerenDAL
     {
         public List<Student> GetAllStudents()
         {
-            string query = "SELECT StudentId, Name FROM [TABLE]";
+            string query = "SELECT student_ID, class, first_name, last_name, telephone_number, roomID FROM dbo.students";
             SqlParameter[] sqlParameters = new SqlParameter[0];
             return ReadTables(ExecuteSelectQuery(query, sqlParameters));
         }
@@ -22,8 +22,12 @@ namespace SomerenDAL
             {
                 Student student = new Student()
                 {
-                    student_ID = (int)dr["StudentId"],
-                    first_name = dr["Name"].ToString()
+                    StudentID = (int)dr["student_ID"],
+                    Class = dr["class"].ToString(),
+                    FirstName = dr["first_name"].ToString(),
+                    LastName = dr["last_name"].ToString(),
+                    TelephoneNumber = dr["telephone_number"].ToString(),
+                    RoomID = (int)dr["roomID"]
                 };
                 students.Add(student);
             }
