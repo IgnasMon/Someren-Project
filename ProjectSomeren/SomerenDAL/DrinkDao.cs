635d627774d477bb889b3db77a3b9faac0c636ad
Zoran Serdic <35991493+ZoranSerdic@users.noreply.github.com>
2023-03-21 12:36:33 +0100

    Added Drink code, still needs to be tested

---
+++ ProjectSomeren/SomerenDAL/DrinkDao.cs
@@ -0,0 +1,39 @@
+﻿using SomerenModel;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenDAL
+{
+    public class DrinkDao : BaseDao
+    {
+        public List<Drink> GetAllDrinks()
+        {
+            string query = "SELECT drink_ID, [name], is_alcoholic, price FROM dbo.drinks";
+            SqlParameter[] sqlParameters = new SqlParameter[0];
+            return ReadTables(ExecuteSelectQuery(query, sqlParameters));
+        }
+
+        private List<Drink> ReadTables(DataTable dataTable)
+        {
+            List<Drink> drinks = new List<Drink>();
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                Drink drink = new Drink()
+                {
+                    Drink_ID = (int)dr["drink_ID"],
+                    Name = dr["name"].ToString(),
+                    Is_Alcoholic = (bool)dr["is_alcoholic"],
+                    Price = (decimal)dr["price"]
+                };
+                drinks.Add(drink);
+            }
+            return drinks;
+        }
+    }
+}
---
+++ ProjectSomeren/SomerenModel/Drink.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenModel
+{
+    public class Drink
+    {
+        public int Drink_ID { get; set; }     // database Primary Key
+        public string Name { get; set; }
+        public bool Is_Alcoholic { get; set; }
+        public decimal Price { get; set; }
+    }
+}
