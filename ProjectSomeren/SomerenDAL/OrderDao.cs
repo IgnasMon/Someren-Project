a92a7739e72c2e0ef069586d8558128edd728de6
IgnasMon <95746420+IgnasMon@users.noreply.github.com>
2023-03-22 23:53:48 +0100

    Revenue file update to Order, DB data shown on ListView

---
+++ ProjectSomeren/SomerenDAL/OrderDao.cs
@@ -0,0 +1,41 @@
+﻿using SomerenModel;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenDAL
+{
+    public class OrderDao : BaseDao
+    {
+        public List<Order> GetAllOrders()
+        {
+            string query = "SELECT [order_ID], [student_ID], [drink_ID], [voucher], [date]" +
+                            "FROM dbo.orders";
+            SqlParameter[] sqlParameters = new SqlParameter[0];
+            return ReadTables(ExecuteSelectQuery(query, sqlParameters));
+        }
+
+        private List<Order> ReadTables(DataTable dataTable)
+        {
+            List<Order> orders = new List<Order>();
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                Order order = new Order()
+                {
+                    ID = (int)dr["order_ID"],
+                    StudentID = (int)dr["student_ID"],
+                    DrinkID = (int)dr["drink_ID"],
+                    Voucher = (bool)dr["voucher"],
+                    OrderDate = DateTime.Parse(dr["date"].ToString()),
+                };
+                orders.Add(order);
+            }
+            return orders;
+        }
+    }
+}
---
+++ ProjectSomeren/SomerenService/OrderService.cs
@@ -0,0 +1,26 @@
+﻿using SomerenDAL;
+using SomerenModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenService
+{
+    public class OrderService
+    {
+        private OrderDao orderdb;
+
+        public OrderService()
+        {
+            orderdb = new OrderDao();
+        }
+
+        public List<Order> GetOrders()
+        {
+            List<Order> orders = orderdb.GetAllOrders();
+            return orders;
+        }
+    }
+}
