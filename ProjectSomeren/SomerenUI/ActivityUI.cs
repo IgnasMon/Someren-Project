51192ac2fb94ada384c643348793f3047b3cfd99
IgnasMon <95746420+IgnasMon@users.noreply.github.com>
2023-04-07 17:05:03 +0200

    ActivityCRUD Validations

--- ProjectSomeren/SomerenUI/ActivityUI.cs
+++ ProjectSomeren/SomerenUI/ActivityUI.cs
@@ -52,7 +52,7 @@ namespace SomerenUI
             // clear the listview before filling it
             listViewActivities.Items.Clear(); // Ignas Assignment 4 - changed so it only clears Items not everything
 
-            //Ignas Assignement 4 - Created a method for loading top part of the ListView which is called from ShowActivitiesPanel
+            //Ignas Assignment 4 - Created a method for loading top part called Load Activities
 
             foreach (Activity activity in activities)
             {
@@ -67,154 +67,264 @@ namespace SomerenUI
         }
         // Activity - End
 
-        // ActivityCRUD - Ignas - Assignment 4 
-        //Load the listView with all column names that will be present
-        private void LoadActivities()
-        {
-            // Full clear of ListView
-            listViewActivities.Clear();
+    #region ActivityCRUD - Ignas - Assignment 4 
 
-            // Ignas Assignment 4 - Changed a few settings
-            // Create column names at the top of the ListViewItem
-            listViewActivities.Columns.Add("ID", 60);                   // Column name "Activity ID" -> "ID" and Size 100 -> 60
-            listViewActivities.Columns.Add("Description", 150);         // Size 160 -> 150
-            listViewActivities.Columns.Add("Start Date Time", 150);     // Size 160 -> 150
-            listViewActivities.Columns.Add("End Date Time", 150);       // Size 160 -> 150
-        }
-
-        // Made the refresh button functional
-        private void refreshToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            listViewActivities.Items.Clear();
-            RefreshActivitiesPanel();
-        }
-
-        // Created a button for Creating a new Activity
-        private void btnCreate_Click(object sender, EventArgs e)
-        {
-            AlertBox(IndentifierCreate, "Create a new activity?", "CREATE ALERT!");
-        }
-
-        // Created a button for Updating/Altering a selected and existing Activity
-        private void btnUpdate_Click(object sender, EventArgs e)
-        {
-            AlertBox(IndentifierUpdate, "Update selected activity?", "UPDATE ALERT!");
-        }
-
-        // Created a button for Deleting Activity
-        private void btnDelete_Click(object sender, EventArgs e)
-        {
-            AlertBox(IndentifierRemove, "DELETE selected activity?", "DELETE ALERT!");
-        }
-
-
-        private void AddNewActivity()
-        {
-            ActivityService activityService = new ActivityService();
-            try
+        #region Load Table
+            //Load the listView with all column names that will be present
+            private void LoadActivities()
             {
-                activityService.AddNewActivity(GetActivityCRUD(IndentifierCreate));
-            } catch (Exception ex)
-            {
-                throw new Exception($"Problem when creating {ex}");
+                // Full clear of ListView
+                listViewActivities.Clear();
+
+                // Ignas Assignment 4 - Changed a few settings
+                // Create column names at the top of the ListViewItem
+                listViewActivities.Columns.Add("ID", 60);                   // Column name "Activity ID" -> "ID" and Size 100 -> 60
+                listViewActivities.Columns.Add("Description", 150);         // Size 160 -> 150
+                listViewActivities.Columns.Add("Start Date Time", 150);     // Size 160 -> 150
+                listViewActivities.Columns.Add("End Date Time", 150);       // Size 160 -> 150
             }
-        }
+        #endregion
 
-        private void UpdateActivity()
-        {
-            ActivityService activityService = new ActivityService();
-            try
+        #region Method Selecting Activity
+            private void listViewActivities_SelectedIndexChanged(object sender, EventArgs e)
             {
-                activityService.UpdateActivity(GetActivityCRUD(IndentifierUpdate));
+                // if there is no activity selected, then return...
+                ClearCRUDPanel();
+                if (listViewActivities.SelectedItems.Count <= 0)
+                    return;
+
+                Activity selectedActivity = (Activity)listViewActivities.SelectedItems[0].Tag;
+
+                textBoxActivityCRUDID.Text = selectedActivity.Activity_ID.ToString();
+                textBoxActivityCRUDDesc.Text = selectedActivity.Description;
+                dateTimePickerStart.Text = selectedActivity.StartDateTime.ToString();
+                dateTimePickerEnd.Text = selectedActivity.EndDateTime.ToString();
             }
-            catch (Exception ex)
+        #endregion
+
+        #region Method GetActivityCRUD
+            // Takes the information from textBoxes and dateTimePickers and creates an Activity out of it, using Activity Model
+            private Activity GetActivityCRUD(int indentifier)
             {
-                throw new Exception($"Problem when updating {ex}");
-            }
-        }
+                Activity activity = new Activity();
 
-        private void RemoveActivity()
-        {
-            ActivityService activityService = new ActivityService();
-            try
-            {
-                activityService.RemoveActivity(GetActivityCRUD(IndentifierRemove));
-            }
-            catch (Exception ex)
-            {
-                throw new Exception($"Problem when deleting {ex}");
-            }
-        }
-
-        private void listViewActivities_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            // if there is no activity selected, then return...
-            ClearCRUDPanel();
-            if (listViewActivities.SelectedItems.Count <= 0)
-                return;
-
-            Activity selectedActivity = (Activity)listViewActivities.SelectedItems[0].Tag;
-
-            textBoxActivityCRUDID.Text = selectedActivity.Activity_ID.ToString();
-            textBoxActivityCRUDDesc.Text = selectedActivity.Description;
-            dateTimePickerStart.Text = selectedActivity.StartDateTime.ToString();
-            dateTimePickerEnd.Text = selectedActivity.EndDateTime.ToString();
-        }
-
-        private Activity GetActivityCRUD(int indentifier)
-        {
-            Activity activity = new Activity();
-
-            try
-            {
-                if(indentifier != IndentifierCreate)
-                    activity.Activity_ID = int.Parse(textBoxActivityCRUDID.Text);
-
-                activity.Description = textBoxActivityCRUDDesc.Text;
-                activity.StartDateTime = DateTime.Parse(dateTimePickerStart.Text);
-                activity.EndDateTime = DateTime.Parse(dateTimePickerEnd.Text);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception($"Problem while converting CRUD data to Model item:\n", ex);
-            }
-
-            return activity;
-        }
-
-        private void ClearCRUDPanel()
-        {
-            textBoxActivityCRUDID.Text = "";
-            textBoxActivityCRUDDesc.Text = "";
-            dateTimePickerStart.Text = DateTime.Now.ToString();
-            dateTimePickerEnd.Text = DateTime.Now.ToString();
-        }
-
-        private void AlertBox(int indentifier, string text, string caption)
-        {
-            DialogResult alertResult = MessageBox.Show($"Are you sure you want to {text}", caption, buttons: MessageBoxButtons.YesNo);
-            if (alertResult == DialogResult.Yes)
-            {
                 try
                 {
-                    if(indentifier == IndentifierCreate)
-                        AddNewActivity();
-                    if (indentifier == IndentifierUpdate)
-                        UpdateActivity();
-                    if (indentifier == IndentifierRemove)
-                        RemoveActivity();
+                    if(indentifier != IndentifierCreate)
+                        activity.Activity_ID = int.Parse(textBoxActivityCRUDID.Text);
 
-                    RefreshActivitiesPanel();
-                    ClearCRUDPanel();
+                    activity.Description = textBoxActivityCRUDDesc.Text;
+                    activity.StartDateTime = DateTime.Parse(dateTimePickerStart.Text);
+                    activity.EndDateTime = DateTime.Parse(dateTimePickerEnd.Text);
                 }
                 catch (Exception ex)
                 {
-                    MessageBox.Show(ex.Message);
+                    throw new Exception($"Problem while converting CRUD data to Model item:\n", ex);
+                }
+
+                return activity;
+            }
+        #endregion
+
+        #region Method ClearCRUDPanel
+            // clears ActivityCRUDPanel
+            private void ClearCRUDPanel()
+            {
+                textBoxActivityCRUDID.Text = "";
+                textBoxActivityCRUDDesc.Text = "";
+                dateTimePickerStart.Text = DateTime.Now.ToString();
+                dateTimePickerEnd.Text = DateTime.Now.ToString();
+            }
+        #endregion
+
+        #region Method AlertBox
+            // AlertBox is for confirming or denying the Creation/Update/Removal of Activity
+            private void AlertBox(int indentifier, string text, string caption)
+            {
+                DialogResult alertResult = MessageBox.Show($"Are you sure you want to {text}", caption, buttons: MessageBoxButtons.YesNo);
+                if (alertResult == DialogResult.Yes)
+                {
+                    try
+                    {
+                        if (textBoxActivityCRUDID.Text == "")
+                        {
+                            if (indentifier == IndentifierCreate)
+                                if (ValidationCreate())
+                                {
+                                    AddNewActivity();
+                                    MessageBox.Show("Success!", caption);
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Action cancelled! Possible problems: \n 1. An activity was selected \n 2. Start Date is later than End Date", caption);
+                                    return;
+                                }
+                            if (indentifier == IndentifierUpdate || indentifier == IndentifierRemove)
+                                MessageBox.Show("Action cancelled! Possible problems: \n 1. Activity was not selected", caption);
+                        }
+                        else
+                        {
+                            if (indentifier == IndentifierUpdate)
+                                if (ValidationUpdate(GetActivityCRUD(IndentifierUpdate),
+                                                        (Activity)listViewActivities.SelectedItems[0].Tag))
+                                {
+                                    UpdateActivity();
+                                    MessageBox.Show("Success!", caption);
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Action cancelled! Possible problems: \n 1. Nothing was changed, \n 2. Start Date is later than End Date", caption);
+                                    return;
+                                }
+
+                            if (indentifier == IndentifierRemove)
+                                if (ValidationRemove(GetActivityCRUD(IndentifierUpdate),
+                                                        (Activity)listViewActivities.SelectedItems[0].Tag))
+                                {
+                                    RemoveActivity();
+                                    MessageBox.Show("Success!", caption);
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Action cancelled! Possible problems: \n 1. Activity was not selected", caption);
+                                    return;
+                                }
+                        }
+
+                        RefreshActivitiesPanel();
+                        ClearCRUDPanel();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+                else if (alertResult == DialogResult.No)
+                    MessageBox.Show("Action was cancelled!");
+            }
+        #endregion
+
+        #region Methods Validation
+
+            #region Method ValidationCreate
+                private bool ValidationCreate()
+                {
+                    if ((DateTime.Parse(dateTimePickerStart.Text) <= DateTime.Parse(dateTimePickerEnd.Text)))
+                        return true;
+
+                    return false;
+                }
+            #endregion
+
+            #region Method ValidationUpdate
+                private bool ValidationUpdate(Activity editedActivity, Activity selectedActivity)
+                {
+                    if ((editedActivity.Activity_ID == selectedActivity.Activity_ID) &&
+                        (
+                            ((int)String.Compare(editedActivity.Description, selectedActivity.Description) == 1) ||
+                            (editedActivity.StartDateTime != selectedActivity.StartDateTime) ||
+                            (editedActivity.EndDateTime != selectedActivity.EndDateTime)
+                        ) &&
+                        (editedActivity.StartDateTime <= editedActivity.EndDateTime)
+                        )
+                        return true;
+
+                    return false;
+                }
+            #endregion
+
+            #region Method ValidationRemove
+                private bool ValidationRemove(Activity removeActivity, Activity selectedActivity)
+                {
+                    if (removeActivity.Activity_ID == selectedActivity.Activity_ID)
+                        return true;
+
+                    return false;
+                }
+            #endregion
+
+        #endregion
+
+        #region RefreshButton
+        // Refresh button refreshes the Table information
+        private void refreshToolStripMenuItem_Click(object sender, EventArgs e)
+            {
+                listViewActivities.Items.Clear();
+                RefreshActivitiesPanel();
+            }
+        #endregion
+
+        #region CreateButton
+            // Create button for Creating/Adding a new Activity
+            private void btnCreate_Click(object sender, EventArgs e)
+            {
+                AlertBox(IndentifierCreate, "Create a new activity?", "CREATE ALERT!");
+            }
+        #endregion
+
+        #region Method AddActivity
+            private void AddNewActivity()
+            {
+                ActivityService activityService = new ActivityService();
+                try
+                {
+                    activityService.AddNewActivity(GetActivityCRUD(IndentifierCreate));
+                } catch (Exception ex)
+                {
+                    throw new Exception($"Problem when creating {ex}");
                 }
             }
-            else if (alertResult == DialogResult.No)
-                MessageBox.Show("Action was cancelled!");
-        }
-        // ActivityCRUD - END
+        #endregion
+
+        #region UpdateButton
+            // Update button for Updating/Altering a selected and existing Activity
+            private void btnUpdate_Click(object sender, EventArgs e)
+            {
+                AlertBox(IndentifierUpdate, "Update selected activity?", "UPDATE ALERT!");
+            }
+        #endregion
+
+        #region Method UpdateActivity
+
+            private void UpdateActivity()
+            {
+                ActivityService activityService = new ActivityService();
+                try
+                {
+                    activityService.UpdateActivity(GetActivityCRUD(IndentifierUpdate));
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception($"Problem when updating {ex}");
+                }
+            }
+
+        #endregion
+
+        #region DeleteButton
+            // Delete button for deleting already existing Activity
+            private void btnDelete_Click(object sender, EventArgs e)
+            {
+                AlertBox(IndentifierRemove, "DELETE selected activity?", "DELETE ALERT!");
+            }
+        #endregion
+
+        #region Method RemoveActivity
+            private void RemoveActivity()
+            {
+                ActivityService activityService = new ActivityService();
+                try
+                {
+                    activityService.RemoveActivity(GetActivityCRUD(IndentifierRemove));
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception($"Problem when deleting {ex}");
+                }
+            }
+        #endregion
+
+    #endregion ActivityCRUD - END
+
     }
 }
