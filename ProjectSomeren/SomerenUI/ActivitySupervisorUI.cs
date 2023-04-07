28a5bf8bb132f6fec45be32fe6afce20208aa66c
sagyM12 <soss.moh50@gmail.com>
2023-04-08 00:03:25 +0100

    activity supervisors

--- ProjectSomeren/SomerenUI/ActivitySupervisorUI.cs
+++ ProjectSomeren/SomerenUI/ActivitySupervisorUI.cs
@@ -16,5 +16,27 @@ namespace SomerenUI
         {
             InitializeComponent();
         }
+
+        private void ActivitySupervisorUI_Load(object sender, EventArgs e)
+        {
+            void activitiesToolStripMenuItem_Click(object sender, EventArgs e)
+            {
+                 ActivityUI activity = new ActivityUI();
+                activity.ShowDialog();
+            }
+
+            void cashRegisterToolStripMenuItem_Click(object sender, EventArgs e)
+            {
+                CashRegisterUI cashRegisterUI = new CashRegisterUI();
+                cashRegisterUI.ShowDialog();
+            }
+        
+             void activitySupervisorsToolStripMenuItem_Click(object sender, EventArgs e)
+            {
+                ActivitySupervisorUI ActivitySupervisorUI = new ActivitySupervisorUI();
+                ActivitySupervisorUI.ShowDialog();
+            }
+            }
+            
     }
 }
